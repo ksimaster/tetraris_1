fa0fc8f12432de10319751501a1f3376ac2274cb
ksimaster <ksimasterlord@gmail.com>
2021-11-20 16:14:26 +0300

    Доработка передвижения волка(флип, анимация с аниматором)

--- tetraris_1/Assets/Scripts/WolfScript.cs
+++ tetraris_1/Assets/Scripts/WolfScript.cs
@@ -7,18 +7,43 @@ public class WolfScript : MonoBehaviour
     private Vector2 direction;
     [SerializeField]
     float speed = 2.5f;
+   // bool toWalk = false;
+    public Animator wolfAnimator;
 
     private void FixedUpdate()
     {
-        direction = Vector2.zero; // (0, 0)
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) // (-1, 0)
-            direction = Vector2.left;
 
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) // (1, 0)
-            direction = Vector2.right;
+        direction = Vector2.zero; // (0, 0)
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        { 
+            direction = Vector2.left; // (-1, 0)
+            wolfAnimator.SetBool("toWalk", true);
+            Flip();
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = Vector2.right;// (1, 0)
+            wolfAnimator.SetBool("toWalk", true);
+            Flip();
+        }
+            
 
         direction *= speed;
         gameObject.GetComponent<Rigidbody2D>().velocity = direction;
+
+        if (direction == Vector2.zero)
+        {
+            wolfAnimator.SetBool("toWalk", false);
+        }
+    }
+
+    public void Flip()
+    {
+        if (direction.x > 0)
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        if (direction.x < 0)
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
     }
 
 
