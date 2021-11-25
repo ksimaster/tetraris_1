eb953231fcce16b580ae78cb8e05f8d9ac2e6e87
IIEC-kycaka <34373675+IIEC-kycaka@users.noreply.github.com>
2021-11-25 11:02:34 +0300

    Добавлено MainMenu в простом виде
    1

---
+++ tetraris_1/Assets/Scripts/MainMenu/OptionsMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+  public AudioMixer audioMixer;
+
+        public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20); 
+    }
+
+    public void SetQuality (int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void Sound()
+    {
+        AudioListener.pause = !AudioListener.pause;
+
+    }
+}
