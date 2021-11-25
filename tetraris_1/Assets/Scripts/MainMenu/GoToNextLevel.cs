b7900bb39d17945d09c82e22e86162e368c508dc
IIEC-kycaka <34373675+IIEC-kycaka@users.noreply.github.com>
2021-11-25 16:10:28 +0300

    Сохранка на префах но я еще думаю

---
+++ tetraris_1/Assets/Scripts/MainMenu/GoToNextLevel.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToNextLevel : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+
+    {
+        if (collision.CompareTag("Player"))
+        {
+            UnLockLevel();
+            SceneManager.LoadScene(1);
+        }
+    }
+
+
+    public void UnLockLevel()
+    {
+        int currentLevel = SceneManager.GetActiveScene().buildIndex;
+        if (currentLevel >= PlayerPrefs.GetInt("levels"))
+        {
+            PlayerPrefs.SetInt("levels", currentLevel + 1);
+        }
+    }
+
+}
