d2148b9050b89dcd231a78905acafe66b85f0d39
ksimaster <ksimasterlord@gmail.com>
2021-12-17 21:22:24 +0300

    Установлен прыжок
    Прыжок стоит, требуется донастройка и тест. Возможно пересборка анимации прыжка

---
+++ tetraris_1/Assets/Scripts/GroundDetection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundDetection : MonoBehaviour
+{
+    public bool isGrounded;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Road"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Road"))
+        {
+            isGrounded = false;
+        }
+    }
+
+
+
+
+}
--- tetraris_1/Assets/Scripts/WolfScript.cs
+++ tetraris_1/Assets/Scripts/WolfScript.cs
@@ -8,17 +8,22 @@ public class WolfScript : MonoBehaviour
     [SerializeField]
     float speed = 2.5f;
    // bool toWalk = false;
-    public Animator wolfAnimator;
+    public Animator animatorWolf;
     public GameObject soundSteps;
+    public Rigidbody2D rigidbodyWolf;
+    public float force;
+    public GroundDetection groundDetection;
+    private bool isJumping;
 
     private void FixedUpdate()
     {
-
+        animatorWolf.SetBool("isGrounded", groundDetection.isGrounded);
+        isJumping = isJumping && !groundDetection.isGrounded;
         direction = Vector2.zero; // (0, 0)
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         { 
             direction = Vector2.left; // (-1, 0)
-            wolfAnimator.SetBool("toWalk", true);
+            animatorWolf.SetBool("toWalk", true);
             Flip();
             soundSteps.GetComponent<AudioSource>().mute = false; // Нет проверки на общее отключение звука
         }
@@ -26,18 +31,26 @@ public class WolfScript : MonoBehaviour
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
             direction = Vector2.right;// (1, 0)
-            wolfAnimator.SetBool("toWalk", true);
+            animatorWolf.SetBool("toWalk", true);
             Flip();
             soundSteps.GetComponent<AudioSource>().mute = false; // Нет проверки на общее отключение звука
         }
-            
+
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && groundDetection.isGrounded)
+        {
+            rigidbodyWolf.AddForce(transform.up * force, ForceMode2D.Impulse);
+            animatorWolf.SetTrigger("StartJump");
+            isJumping = true;
+        }
+
 
         direction *= speed;
-        gameObject.GetComponent<Rigidbody2D>().velocity = direction;
+        direction.y = rigidbodyWolf.velocity.y;
+        rigidbodyWolf.velocity = direction;
 
         if (direction == Vector2.zero)
         {
-            wolfAnimator.SetBool("toWalk", false);
+            animatorWolf.SetBool("toWalk", false);
             soundSteps.GetComponent<AudioSource>().mute = true; // Нет проверки на общее отключение звука
         }
     }
