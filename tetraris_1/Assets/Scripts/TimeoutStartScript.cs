3856b898baa3280d4dd429b7de98a74cb578861d
ksimaster <ksimasterlord@gmail.com>
2022-01-14 21:16:14 +0300

    Правки 5го уровня

--- tetraris_1/Assets/Scripts/TimeoutStartScript.cs
+++ tetraris_1/Assets/Scripts/TimeoutStartScript.cs
@@ -13,27 +13,26 @@ public class TimeoutStartScript : MonoBehaviour
 
     public void OnStart()
     {
-        Debug.Log("Старт корутины");
+        
         StartCoroutine("Timeout");
-        Debug.Log("Завершение корутины");
-        StopAllCoroutines();
-        Debug.Log("Остановлены все корутины");
+       
+
         TimeoutObject.SetActive(true);
         for (int i=1; i<10; i++)
         {
             if (i % 2 == 0)
             {
-                Debug.Log("Выключение объекта");
+                
                 TimeoutObject.SetActive(false);
-                Debug.Log("Старт корутины в цикле");
+                
                 StartCoroutine("Timeout");
-                Debug.Log("Завершение корутины в цикле");
-                StopAllCoroutines();
-                Debug.Log("Остановлены все корутины в цикле");
+                
+
             }
             else
             {
                 TimeoutObject.SetActive(true);
+                StartCoroutine("Timeout");
             }
         }
     }
