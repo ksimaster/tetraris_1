25fcdf6331bd1f37b6e545dcd37eb72b19c3f49f
ksimaster <36484548+ksimaster@users.noreply.github.com>
2022-02-20 18:55:14 +0300

    Скрипт на прыжок на 10м

--- tetraris_1/Assets/Scripts/WolfRunScript.cs
+++ tetraris_1/Assets/Scripts/WolfRunScript.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 
 public class WolfRunScript : MonoBehaviour
 {
-    private Vector2 direction;
-    [SerializeField]
-    float speed = 2.5f;
+    
+    
    // bool toWalk = false;
     public Animator animatorWolf;
     public GameObject soundSteps;
@@ -18,7 +17,7 @@ public class WolfRunScript : MonoBehaviour
     public Vector2 legsPosition;
     public Vector2 headPosition;
 
-    private bool isJumping;
+    private bool isJumping = false;
 
     private void Awake()
     {
@@ -28,73 +27,34 @@ public class WolfRunScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        animatorWolf.SetBool("isGrounded", groundDetection.isGrounded);
-        isJumping = isJumping && !groundDetection.isGrounded;
-        animatorWolf.SetBool("toJump", isJumping);
-        if (!isJumping)direction = Vector2.zero; // (0, 0) || добавлена проверка на наличие прыжка
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if(transform.position.y >= -3.6)
         {
-            isJumping = false;
-            animatorWolf.SetBool("toJump", isJumping);
-            direction = Vector2.left; // (-1, 0)
-            animatorWolf.SetBool("toWalk", true);
-            Flip();
-            soundSteps.GetComponent<AudioSource>().mute = false; // Нет проверки на общее отключение звука
-        }
-
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            isJumping = false;
-            animatorWolf.SetBool("toJump", isJumping);
-            direction = Vector2.right;// (1, 0)
-            animatorWolf.SetBool("toWalk", true);
-            Flip();
-            soundSteps.GetComponent<AudioSource>().mute = false; // Нет проверки на общее отключение звука
-        }
-
-        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow)) && groundDetection.isGrounded)
-        {
-            rigidbodyWolf.AddForce(new Vector2(0, force));//(transform.up * force, ForceMode2D.Impulse); с использованием force mode
-            // animatorWolf.SetTrigger("StartJump");
             isJumping = true;
-            animatorWolf.SetBool("toJump", isJumping);
+        }
+        else
+        {
+            isJumping = false;
+        }
+
+        if(groundDetection.isGrounded) animatorWolf.SetTrigger("toRun");
+
+        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.UpArrow))&&!isJumping)// && groundDetection.isGrounded)
+        {
+            animatorWolf.SetTrigger("StartJump");
+            rigidbodyWolf.AddForce(new Vector2(0, force));//(transform.up * force, ForceMode2D.Impulse); с использованием force mode
+       
+            Invoke("Run", 0.7f);
             
         }
 
 
-        direction *= speed;
-        direction.y = rigidbodyWolf.velocity.y;
-        rigidbodyWolf.velocity = direction;
 
-        if (direction == Vector2.zero)
-        {
-            animatorWolf.SetBool("toWalk", false);
-            soundSteps.GetComponent<AudioSource>().mute = true; // Нет проверки на общее отключение звука
-        }
     }
 
-    public void Flip()
+    void Run()
     {
-        if (direction.x > 0)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
-            if(legs.transform.localPosition.x != legsPosition.x && head.transform.localPosition.x != headPosition.x) { 
-                gameObject.transform.position = new Vector2(gameObject.transform.position.x + 0.6f, gameObject.transform.position.y);
-                legs.transform.localPosition = legsPosition;
-                head.transform.localPosition = headPosition; 
-            }
-        }
-            
-        if (direction.x < 0)
-        {
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
-            if(legs.transform.localPosition.x == legsPosition.x && head.transform.localPosition.x == headPosition.x) { 
-                gameObject.transform.position = new Vector2(gameObject.transform.position.x - 0.6f, gameObject.transform.position.y);
-                legs.transform.localPosition = new Vector2(legs.transform.localPosition.x + 0.634f, legs.transform.localPosition.y);
-                head.transform.localPosition = new Vector2(head.transform.localPosition.x + 0.289f, head.transform.localPosition.y);
-            }
-        }
-            
+
+        animatorWolf.SetTrigger("toRun");
     }
 
 
