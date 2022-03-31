986f11a19dd27c7889058b1b032a3c5972dbc28a
ksimaster <ksimasterlord@gmail.com>
2022-03-31 21:14:21 +0300

    Префы для костей на левелах и 5м

--- tetraris_1/Assets/Scripts/CollectingScript.cs
+++ tetraris_1/Assets/Scripts/CollectingScript.cs
@@ -9,19 +9,19 @@ public class CollectingScript : MonoBehaviour
     public Text textScore;
     private int score;
 
-
-    
-    void Update()
+    private void Start()
     {
-        
+        score = PlayerPrefs.GetInt("score"); // на старте загружаем очки
+        textScore.text = score.ToString();
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        // Debug.Log(col.gameObject.name);
+        //Debug.Log(col.gameObject.name);
         if (col.gameObject.CompareTag(collectTag))
         {
             score++;
+            PlayerPrefs.SetInt("score", score);
             textScore.text = score.ToString();
             Destroy(col.gameObject);
             
